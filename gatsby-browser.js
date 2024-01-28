ba23a419da940bdc8182efcdd947d0a2455ded59
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2024-01-28 14:22:12 -0600

    Add google tag manager

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,5 +6,15 @@ export const wrapRootElement = ({ element }) => (
   <>
     <GlobalStyles />
     {element}
+
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-5MZV6SVR"
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+        title="gtag-no-script"
+      />
+    </noscript>
   </>
 );
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,5 +6,29 @@ export const wrapRootElement = ({ element }) => (
   <>
     <GlobalStyles />
     {element}
+
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-5MZV6SVR"
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+        title="gtag-no-script"
+      />
+    </noscript>
   </>
 );
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5MZV6SVR');`,
+      }}
+    />,
+  ]);
+};
